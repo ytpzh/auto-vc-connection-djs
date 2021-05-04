000b919a6d7b8c3408395485d5919d7f048f5b5a
PlayZoneHappier <contact@pzh.fr>
2021-05-04 14:10:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var client = new Discord.Client();
 client.login(process.env.TOKEN);
 
 client.on("ready", () => {
-    let channel = client.channels.get("539783832010752015");
+    let channel = client.channels.get("channel id");
     if(!channel) return console.log("Channel introuvable.");
     channel.join().then(connection => { console.log(`Connecré au salon ${channel}`);
     }).catch(e => {console.error(e)});
