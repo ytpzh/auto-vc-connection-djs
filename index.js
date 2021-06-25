6045a3fb392c4fe565d3e295ad82f4f5a8cfca76
PZH <contact@pzh.fr>
2021-06-25 10:51:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ client.login("token");
 client.on("ready", () => {
     let channel = client.channels.cache.get("channel id");
     if(!channel) return console.log("Channel introuvable.");
-    channel.join().then(connection => { console.log(`Connecté au salon ${channel}`);
+    channel.join().then(() => { console.log(`Connecté au salon ${channel}`);
     }).catch(e => {console.error(e)});
 });
